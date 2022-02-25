efdea9252af8f0e5f32b1c7983c8bf775daa9f08
richbrill <3015680+richbrill@users.noreply.github.com>
2022-02-25 21:49:47 +0000

    Additional info for debugging purposes

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,8 @@ import { Engine, Bodies, Composite, Render, Runner, Common } from 'matter-js';
 var engine = Engine.create();
 
 // Add poly-decomp, uncommenting this causes this error "Uncaught TypeError: Cannot read properties of undefined (reading 'type')"
-//Common.setDecomp(require('poly-decomp'));
+// but only when using the Clockwise concave polygon below
+Common.setDecomp(require('poly-decomp'));
 
 // create a renderer
 var render = Render.create({
@@ -12,18 +13,29 @@ var render = Render.create({
   engine: engine
 });
 
-// Create a concave polygon
+// Clockwise concave polygon
+// const vertices = [
+//   {x: 8, y: 9}, 
+//   {x: 86, y: 28},
+//   {x: 95, y: 77},
+//   {x: 76, y: 158},
+//   {x: 35, y: 137},
+//   {x: 28, y: 69},
+//   {x: 8, y: 9}
+// ]
+
+// Counter-Clockwise concave polygon (comment this out and uncomment the above Clockwise polygon to see the error)
 const vertices = [
-  {x: 8, y: 9}, 
-  {x: 86, y: 28},
-  {x: 95, y: 77},
-  {x: 76, y: 158},
-  {x: 35, y: 137},
+  {x: 8, y: 9},
   {x: 28, y: 69},
+  {x: 35, y: 137},
+  {x: 76, y: 158},
+  {x: 95, y: 77},
+  {x: 86, y: 28},
   {x: 8, y: 9}
 ]
 
-const body = Bodies.fromVertices(600, 200, vertices, { isStatic: true }, true, 0.6, 5, 0.5);
+const body = Bodies.fromVertices(600, 200, vertices, { isStatic: true }, true, 0.01, 10, 0.01);
 
 // Add body to matter
 Composite.add(engine.world, [
